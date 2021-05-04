c58c5101b55b52bb5ef0638bfc1a6cc1fa670867
Robert J. Kusznier <no-cheating@autistici.org>
2021-05-04 14:32:58 +0200

    Add pagination for children list

--- src/ChildrenDashboard/ChildrenDashboard.js
+++ src/ChildrenDashboard/ChildrenDashboard.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useGetChildren } from '../api';
+import { Pagination } from '../Pagination';
 
 
 function ChildrenDashboard() {
-  const children = useGetChildren();
+  const childrenAll = useGetChildren();
+  const [childrenCurrentPage, setChildrenCurrentPage] = useState([]);
 
   return (
     <>
       <h1>Children</h1>
       <ol>
-        {children.map((child) => (
+        {childrenCurrentPage.map((child) => (
           <li key={child.childId}>
             {child.name.fullName}
           </li>
         ))}
       </ol>
+      <Pagination
+        itemsAll={childrenAll}
+        setItemsCurrentPage={setChildrenCurrentPage}
+      />
     </>
   );
 }
---
+++ src/Pagination/Pagination.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useMemo, useState } from 'react';
+
+import "./Pagination.css";
+
+
+const ITEMS_PER_PAGE_OPTIONS = [10, 25, 50, 100];
+const ITEMS_PER_PAGE_DEFAULT = 25;
+
+
+function Pagination({
+  itemsAll,
+  itemsPerPage = ITEMS_PER_PAGE_DEFAULT,
+  setItemsCurrentPage,
+}) {
+  const totalPages = Math.ceil(itemsAll.length / itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const pages = useMemo(function createPagesList() {
+    const pagesList = [];
+    for (let pageIdx = 1; pageIdx <= totalPages; pageIdx += 1) {
+      pagesList.push(pageIdx);
+    }
+    return pagesList;
+  }, [totalPages]);
+
+  useEffect(function createItemsCurrentPage() {
+    const currentPageStart = (currentPage - 1) * itemsPerPage;
+    const currentPageEnd = currentPageStart + itemsPerPage;
+    const itemsCurrentPage = itemsAll.slice(currentPageStart, currentPageEnd);
+    setItemsCurrentPage(itemsCurrentPage);
+  }, [currentPage, itemsAll, itemsPerPage, setItemsCurrentPage]);
+
+  function pageButtonClassName(page) {
+    if (page === currentPage) {
+      return 'Pagination-pageButton Pagination-pageButton--currentPage';
+    } else {
+      return 'Pagination-pageButton';
+    }
+  }
+
+  return (
+    <ol className="Pagination">
+      {pages.map((page) => (
+        <li className="Pagination-page">
+          <button
+            onClick={() => setCurrentPage(page)}
+            className={pageButtonClassName(page)}
+          >
+            {page}
+          </button>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+
+export {
+  Pagination,
+  ITEMS_PER_PAGE_OPTIONS,
+}
