5b61dfc5d7f7c5edfd61ab067631fbe397c406ff
Robert J. Kusznier <no-cheating@autistici.org>
2021-05-04 14:36:42 +0200

    Add some styling and markup to children dashboard

--- src/ChildrenDashboard/ChildrenDashboard.js
+++ src/ChildrenDashboard/ChildrenDashboard.js
@@ -3,26 +3,34 @@ import React, { useState } from 'react';
 import { useGetChildren } from '../api';
 import { Pagination } from '../Pagination';
 
+import './ChildrenDashboard.css';
+
 
 function ChildrenDashboard() {
   const childrenAll = useGetChildren();
   const [childrenCurrentPage, setChildrenCurrentPage] = useState([]);
 
   return (
-    <>
-      <h1>Children</h1>
-      <ol>
-        {childrenCurrentPage.map((child) => (
-          <li key={child.childId}>
-            {child.name.fullName}
-          </li>
-        ))}
-      </ol>
-      <Pagination
-        itemsAll={childrenAll}
-        setItemsCurrentPage={setChildrenCurrentPage}
-      />
-    </>
+    <div className="ChildrenDashboard">
+      <header>
+        <h1>Children</h1>
+      </header>
+      <main>
+        <ol>
+          {childrenCurrentPage.map((child) => (
+            <li key={child.childId}>
+              {child.name.fullName}
+            </li>
+          ))}
+        </ol>
+      </main>
+      <footer>
+        <Pagination
+          itemsAll={childrenAll}
+          setItemsCurrentPage={setChildrenCurrentPage}
+        />
+      </footer>
+    </div>
   );
 }
 
